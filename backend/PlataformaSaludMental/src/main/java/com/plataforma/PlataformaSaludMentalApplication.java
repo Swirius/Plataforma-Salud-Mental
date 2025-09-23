137b5ab1cdd77a74d3ba0a71695ff29de906641d
Swirius <swiridoagustin123@gmail.com>
2025-09-23 20:35:32 -0300

    Proyecto Base

---
+++ backend/PlataformaSaludMental/src/main/java/com/plataforma/PlataformaSaludMentalApplication.java
@@ -0,0 +1,13 @@
+package com.plataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaSaludMentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaSaludMentalApplication.class, args);
+	}
+
+}
